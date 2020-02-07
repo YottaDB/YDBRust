201386b41cf98e82a4922fbf2c3adeff0c348b54
Joshua Nelson <joshua@yottadb.com>
2020-02-07 10:59:41 -0500

    update wordfreq

--- examples/wordfreq.rs
+++ examples/wordfreq.rs
@@ -1,3 +1,6 @@
+//! Find the frequency of words in a file
+//! Expects the file to be named `wordfreq_input.txt` in the current directory.
+
  #[macro_use] extern crate yottadb;
 
 use std::fs::File;
@@ -23,12 +26,12 @@ fn main() -> Result<(), Box<dyn Error>> {
     for word in words {
         let word = word.to_lowercase();
         if word != "" {
-            word_key[1] = Vec::from(word);
+            word_key[0] = Vec::from(word);
             word_key.increment(None)?;
         }
     }
     // Iterate through all words and put them into a global ordered by frequency
-    word_key[1] = Vec::from("");
+    word_key[0] = Vec::from("");
     for k in word_key.iter_subs_values() {
         let (k, v) = k?;
         let word = k;
@@ -41,12 +44,12 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut word_key = make_ckey!(ctx, "^index", "", "");
     for k in freq_key.iter_subs_reverse() {
         let v = k?;
-        word_key[1] = v;
-        word_key[2] = Vec::from("");
+        word_key[0] = v;
+        word_key[1] = Vec::from("");
         for k2 in word_key.iter_key_subs() {
             let k2 = k2?;
-            let v2 = String::from_utf8_lossy(&k2[2]);
-            let v1 = String::from_utf8_lossy(&k2[1]);
+            let v2 = String::from_utf8_lossy(&k2[1]);
+            let v1 = String::from_utf8_lossy(&k2[0]);
             println!("{}\t{}", v1, v2);
         }
     }
