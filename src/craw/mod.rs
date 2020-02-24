3d99e18e9e902bc763c9c997ee4c959f5bf77042
Joshua Nelson <jyn514@gmail.com>
2020-02-24 12:49:00 -0500

    add more documentation comments

--- src/craw/mod.rs
+++ src/craw/mod.rs
@@ -17,11 +17,40 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 // Currently, bindgen doesn't produce calculated values
 //  Add them here as needed
 pub const YDB_INT_MAX: i32 = 0x7fffffff;
+/// The constant used to indicate a [transaction] should be restarted.
+///
+/// # See also
+/// - [`ydb_tp_st`](https://docs.yottadb.com/MultiLangProgGuide/cprogram.html#ydb-tp-s-ydb-tp-st)
+/// - [Simple API docs](https://docs.yottadb.com/MultiLangProgGuide/cprogram.html#normal-return-codes)
+///
+/// [transaction]: https://docs.yottadb.com/MultiLangProgGuide/MultiLangProgGuide.html#transaction-processing
 pub const YDB_TP_RESTART: i32 = (YDB_INT_MAX - 1); /* 0x7ffffffe */
+/// The constant that indicates a [transaction] should be rolled back.
+/// This can be used in conjunction with [`ydb_tp_st`]
+/// to rollback an ongoing transaction.
+///
+/// [transaction]: https://docs.yottadb.com/MultiLangProgGuide/MultiLangProgGuide.html#transaction-processing
+/// [`ydb_tp_st`]: https://docs.yottadb.com/MultiLangProgGuide/cprogram.html#ydb-tp-s-ydb-tp-st
 pub const YDB_TP_ROLLBACK: i32 = (YDB_INT_MAX - 2); /* 0x7ffffffd */
+/// The constant used by [`ydb_node_next_st()`]
+/// to indicate that the end of a node has been reached.
+///
+/// [`ydb_node_next_st()`]: https://docs.yottadb.com/MultiLangProgGuide/cprogram.html#ydb-node-next-s-ydb-node-next-st
 pub const YDB_NODE_END: i32 = (YDB_INT_MAX - 3); /* 0x7ffffffc */
+/// The constant used to indicate that timeout occurred while acquiring a [lock].
+///
+/// # See also
+/// - [`ydb_lock_incr_st`](https://docs.yottadb.com/MultiLangProgGuide/cprogram.html#ydb-incr-s-ydb-incr-st)
+///
+/// [lock]: https://docs.yottadb.com/MultiLangProgGuide/MultiLangProgGuide.html#locks
 pub const YDB_LOCK_TIMEOUT: i32 = (YDB_INT_MAX - 4); /* 0x7ffffffb */
+/// An error code returned by [`ydb_file_is_identical_t()`].
+///
+/// [`ydb_file_is_identical_t()`]: https://docs.yottadb.com/MultiLangProgGuide/cprogram.html#ydb-file-is-identical-ydb-file-is-identical-t
 pub const YDB_NOTOK: i32 = (YDB_INT_MAX - 5); /* 0x7ffffffa */
+/// The constant used to indicate that no [transaction] is ongoing.
+///
+/// [transaction]: https://docs.yottadb.com/MultiLangProgGuide/MultiLangProgGuide.html#transaction-processing
 pub const YDB_NOTTP: u64 = 0;
 
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,27 @@
+//! YottaDB is a NoSQL Database suitable for high-performance systems.
+//!
+//! YottaDB runs in-process, like SQLite, with no need for a daemon.
+//! This crate is a Rust wrapper around the C implementation of YottaDB.
+//!
+//! There are three major APIs:
+//! - `craw`, the FFI bindings generated directly by bindgen.
+//!     These are not recommended for normal use,
+//!     but are available in case the other APIs are missing functionality.
+//! - `simple_api`, a wrapper around the `craw` API
+//!     which handles resizing buffers and various other recoverable errors.
+//!     The simple API also provides a `YDBError` struct so that errors are
+//!     returned as `Result` instead of an error code.
+//! - `context_api`, which is a wrapper around the `simple_api` that
+//!     stores the current tptoken and an error buffer
+//!     so you don't have to keep track of them yourself.
+//!     The reason the context_api is necessary is because this crate binds to
+//!     the threaded version of YottaDB, which requires a `tptoken` and `err_buffer`.
+//!     See [transaction processing] for more details on transactions and `tptoken`s.
+//!
+//! The context_api is recommended for normal use, but the others are available if your
+//! needs are more specialized.
+//!
+//! [transaction processing]: https://docs.yottadb.com/MultiLangProgGuide/MultiLangProgGuide.html#transaction-processing
 pub mod craw;
 pub mod simple_api;
 pub mod context_api;
