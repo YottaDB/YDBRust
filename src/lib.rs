23f60a294d1d568159d3be94a972abb7d05a6ba2
Joshua Nelson <joshua@yottadb.com>
2020-04-03 13:02:41 -0400

    Add copyright notice and check for copyright in CI

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+/****************************************************************
+*                                                               *
+* Copyright (c) 2019-2020 YottaDB LLC and/or its subsidiaries.  *
+* All rights reserved.                                          *
+*                                                               *
+*       This source code contains the intellectual property     *
+*       of its copyright holder(s), and is made available       *
+*       under a license.  If you do not know the terms of       *
+*       the license, please stop and do not read further.       *
+*                                                               *
+****************************************************************/
+
 //! YottaDB is a NoSQL Database suitable for high-performance systems.
 //!
 //! YottaDB runs in-process, like SQLite, with no need for a daemon.
--- test.sh
+++ test.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
+
+#################################################################
+#                                                               #
+# Copyright (c) 2020 YottaDB LLC and/or its subsidiaries.       #
+# All rights reserved.                                          #
+#                                                               #
+#       This source code contains the intellectual property     #
+#       of its copyright holder(s), and is made available       #
+#       under a license.  If you do not know the terms of       #
+#       the license, please stop and do not read further.       #
+#                                                               #
+#################################################################
+
+tests/copyright.sh
 cargo fmt -- --check
 cargo clippy -- --deny clippy::all
 cargo test
