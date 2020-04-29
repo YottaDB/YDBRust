2dd42f97d17ebb7d8606be01a6db3816e2fcf79e
Joshua Nelson <joshua@yottadb.com>
2020-04-29 15:53:45 -0400

    Improve copyright script
    - Allow copyright.sh to be run from anywhere in the repository, not just
    the top-most level
    - Do not require call-in files (i.e. files ending in '.ci') to have a
    copyright notice

--- tests/copyright.sh
+++ tests/copyright.sh
@@ -20,7 +20,8 @@ else
 	COMMIT=HEAD
 fi
 
-MISSING_COPYRIGHT="$(git diff --name-only $COMMIT | xargs --no-run-if-empty grep -L "Copyright (c) .*$(date +%Y) YottaDB" -L)"
+cd "$(git rev-parse --show-toplevel)"
+MISSING_COPYRIGHT="$(git diff --name-only $COMMIT | grep -v '\.ci$' | xargs --no-run-if-empty grep -L "Copyright (c) .*$(date +%Y) YottaDB" -L)"
 if [ -n "$MISSING_COPYRIGHT" ]; then
     echo "the following files do not have an up-to-date copyright:"
     for file in $MISSING_COPYRIGHT; do
