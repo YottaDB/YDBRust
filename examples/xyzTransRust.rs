e6b8a7cdad5182c9cee7561983e53a710c0f5d9a
Joshua Nelson <joshua@yottadb.com>
2020-09-23 13:16:29 -0400

    [YottaDB/DB/YDBDoc#202] Require xyzTransRust to be manually interrupted instead of stopping after 5 seconds
    This makes xyzTransRust consistent with the rest of the xyzTrans
    programs for the Acculturation Guide.
    
    - Set a timeout of 5 seconds in the gitlab pipeline for xyzTransRust
    - Exit with success in the pipeline only if the command timed out

--- examples/xyzTransRust.rs
+++ examples/xyzTransRust.rs
@@ -19,55 +19,16 @@
 
 use std::error::Error;
 use std::time::{Duration, SystemTime};
-use std::sync::atomic::{AtomicBool, Ordering};
 
 use rand::Rng;
 use yottadb::TransactionStatus;
 use yottadb::context_api::{Context, KeyContext};
 
-static KILL_SWITCHES: [AtomicBool; 5] = [
-    AtomicBool::new(false),
-    AtomicBool::new(false),
-    AtomicBool::new(false),
-    AtomicBool::new(false),
-    AtomicBool::new(false),
-];
-
 fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
-    use std::thread;
-
-    let handles: Vec<_> = (0..5).map(|i| thread::spawn(move || trans(i))).collect();
-    println!("finished spawn");
-    thread::sleep(Duration::from_secs(5));
-    println!("finished sleep");
-
-    for switch in &KILL_SWITCHES {
-        // see comment by load below for explanation of ordering
-        switch.store(true, Ordering::SeqCst);
-    }
-    println!("finished kill");
-
-    for handle in handles {
-        handle.join().expect("no thread should panic")?;
-    }
-    println!("all threads joined");
-
-    Ok(())
-}
-
-fn trans(i: usize) -> Result<(), Box<dyn Error + Send + Sync>> {
-    println!("spawned {}", i);
-
     let mut rng = rand::thread_rng();
     let ctx = Context::new();
 
     loop {
-        // this could probably use Relaxed but I don't care about the performance
-        // and I don't want someone to copy it (since Relaxed only works if this
-        // is the _only_ communication between threads).
-        if KILL_SWITCHES[i].load(Ordering::SeqCst) {
-            return Ok(());
-        }
         ctx.tp(
             |ctx| {
                 let us = SystemTime::now()
@@ -76,14 +37,14 @@ fn trans(i: usize) -> Result<(), Box<dyn Error + Send + Sync>> {
                     .to_string();
                 let n: i64 = rng.gen::<i32>().into();
 
-                // ^Delta(ms) = n
+                // ^Delta(us) = n
                 let delta = KeyContext::new(&ctx, "^Delta", &[us.as_bytes()]);
                 delta.set(n.to_string())?;
 
                 // ^Crab(ms) = ^Crab(lasttime) - n
                 let crab = KeyContext::new(&ctx, "^Crab", &[us.as_bytes()]);
                 let last_crab = crab.prev_sub()?;
-                let last_crab_val: i64 = String::from_utf8_lossy(&last_crab.get()?).parse()?;
+                let last_crab_val: i64 = std::str::from_utf8(&last_crab.get()?)?.parse()?;
                 crab.set((last_crab_val - n).to_string())?;
 
                 // ^Horse(ms) = ^Horse(lasttime) + n
