aec43c80827332058382ef479933574b25d63e4b
Narayanan Iyer <nars@yottadb.com>
2022-10-31 10:28:43 -0400

    [YottaDB/DB/YDB#564] Fix ydb_ci_t() unit test in tests/infinite_inv_str.rs now that INVSTRLEN error is not returned
    Background
    ----------
    * The `rust-latest` scheduled pipeline job started failing with the following symptom.
    
    * Below is from an example failure at https://gitlab.com/YottaDB/Lang/YDBRust/-/jobs/3246442440.
    
      ```
      test ydb_ci_t ... FAILED
      failures:
      ---- ydb_ci_t stdout ----
      thread 'ydb_ci_t' panicked at 'called `Result::unwrap_err()` on an `Ok` value: []', tests/infinite_inv_str.rs:40:82
      ```
    
    Issue
    -----
    * The `ydb_ci_t` test expected an `INVSTRLEN` error when the `out_buffer` for a `ydb_ci_t()` SimpleThreadAPI
      call did not have enough space to hold the return value from the call-in.
    
    * This behavior was changed recently as part of YottaDB/DB/YDB#564 (YottaDB/DB/YDB@c3691d3f) to return a
      truncated string instead of issuing an `INVSTRLEN` error.
    
    Fix
    ---
    * The `ydb_ci_t` test is reworked to now expect no `INVSTRLEN` error but instead expect an empty string
      return value.

--- tests/infinite_inv_str.rs
+++ tests/infinite_inv_str.rs
@@ -1,6 +1,6 @@
 /****************************************************************
 *                                                               *
-* Copyright (c) 2019-2021 YottaDB LLC and/or its subsidiaries.  *
+* Copyright (c) 2019-2022 YottaDB LLC and/or its subsidiaries.  *
 * All rights reserved.                                          *
 *                                                               *
 *       This source code contains the intellectual property     *
@@ -15,10 +15,11 @@ use std::convert::TryInto;
 use std::env::set_var;
 use std::ffi::CString;
 use std::os::raw::c_ulong;
+use std::os::raw::c_char;
 
 use yottadb::{
     YDB_NOTTP,
-    craw::{YDB_ERR_INVSTRLEN, YDB_MAX_STR, ydb_string_t},
+    craw::{YDB_MAX_STR, ydb_string_t},
     ci_t,
 };
 
@@ -27,17 +28,18 @@ fn ydb_ci_t() {
     set_var("ydb_routines", "examples/m-ffi");
     set_var("ydb_ci", "examples/m-ffi/calltab.ci");
 
-    let mut out_buffer = Vec::new();
+    let mut out_buffer = Vec::<u8>::new();
     let mut status = ydb_string_t {
-        address: out_buffer.as_mut_ptr() as *mut _,
+        address: out_buffer.as_mut_ptr() as *mut c_char,
         length: out_buffer.capacity() as c_ulong,
     };
     let routine = CString::new("HelloWorld1").unwrap();
     // Allocate a buffer with the maximum capacity so we're sure that any INVSTRLEN is for `out_buffer`, not `err_buffer`.
     let capacity = YDB_MAX_STR.try_into().expect("16-bit platforms are not supported");
     let err_buffer = Vec::with_capacity(capacity);
-    let err = unsafe {
-        ci_t!(YDB_NOTTP, err_buffer, &routine, &mut status as *mut ydb_string_t).unwrap_err()
-    };
-    assert_eq!(err.status, YDB_ERR_INVSTRLEN);
+    unsafe { ci_t!(YDB_NOTTP, err_buffer, &routine, &mut status as *mut ydb_string_t).unwrap() };
+    // After YDB#564, an INVSTRLEN is no longer returned for `out_buffer`. Instead a truncated buffer is returned.
+    // In this case, since `out_buffer` was allocated with 0 bytes to start with, we expect a 0-byte return value.
+    // Hence the assert below against an empty string.
+    assert_eq!(out_buffer, b"");
 }
