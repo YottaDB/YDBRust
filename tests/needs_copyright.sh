dde30f9b832fd213616672336134208a367f69a9
K.S. Bhaskar <bhaskar@yottadb.com>
2025-09-04 16:29:22 -0400

    [#53] License change
    - COPYING contains a copy of the GNU General Public License version 3
    - COPYING.LESSER contains copy of the GNU Lesser General Public License version 3
    - References to an upstream package are removed from LICENSE.
    - `needs_copyright.sh` fixed to also not check for copyrights on above
      files; also file updated to the version used in the Octo repository
      which has more logic in it for different scenarios.
    - Add previously missed COPYING.LESSER.

--- tests/needs_copyright.sh
+++ tests/needs_copyright.sh
@@ -2,7 +2,7 @@
 
 #################################################################
 #								#
-# Copyright (c) 2020 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2020-2025 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -12,8 +12,40 @@
 #								#
 #################################################################
 
-set -euv
-
 # Determines whether a file should need a copyright by its name
 # Returns 0 if it needs a copyright and 1 otherwise.
-! [ "$1" = README.md ]
+# Returns 2 if an error occurs.
+set -eu
+
+if ! [ $# = 1 ]; then
+	echo "usage: $0 <filename>"
+	exit 2
+fi
+
+file="$1"
+
+# Don't require deleted files to have a copyright
+if ! [ -e "$file" ]; then
+       exit 1
+fi
+
+skipextensions="ref png zwr html ci jpg"	# List of extensions that cannot have copyrights.
+	# .png  -> these are images (i.e. binary files) used in the documentation.
+	#		Same reason as .rst for not requiring a copyright.
+	# .ref  -> reference files used by the test cases (e.g. tests/outref/TUF001.ref).
+	#		Those have a fixed format and should not go through copyright changes.
+	# .zwr  -> zwrite format extract file (does not currently allow a comment character).
+	# .html -> there are a couple of files currently under doc/templates which don't need copyrights.
+	# .ci   -> e.g. calltab.ci stores the call-in table which does not currently have a provision for comment characters.
+if echo "$skipextensions" | grep -q -w "$(echo "$file" | awk -F . '{print $NF}')"; then
+	exit 1
+fi
+
+# Below is a list of specific files that do not have a copyright or are from an
+# external project with a specific copyright so ignore them
+skiplist="COPYING COPYING.LESSER README.md LICENSE"
+for skipfile in $skiplist; do
+	if [ "$file" = "$skipfile" ]; then
+		exit 1
+	fi
+done
